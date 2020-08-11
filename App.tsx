73020aed4d0437a55565602655de31c27a1b5597
felipesanderp <felipe.panisson@hotmail.com>
2020-08-11 16:54:52 -0300

    add fonts to app

--- App.tsx
+++ App.tsx
@@ -3,11 +3,26 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
+
+import {
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  useFonts,
+} from '@expo-google-fonts/roboto-slab';
 
 import Routes from './src/routes';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <NavigationContainer>
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
