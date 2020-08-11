90506693938c43050596a473df1ca56fbdbb4ff5
felipesanderp <felipe.panisson@hotmail.com>
2020-08-11 17:51:23 -0300

    add input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+
+export const Container = styled.View`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #232129;
+  border-radius: 10px;
+  margin-bottom: 8px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+  color: #fff;
+  font-size: 16px;
+  font-family: 'RobotoSlab_400Regular';
+`;
+
+export const FeatherIcon = styled(Feather)`
+  margin-right: 16px;
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Title } from './styles';
@@ -13,6 +14,10 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="Senha" />
 
       <Button>Entrar</Button>
     </Container>
