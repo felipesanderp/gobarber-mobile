3f5ae6cfea6515f7b7e225e40fc78c886527360f
felipesanderp <felipe.panisson@hotmail.com>
2020-08-11 15:49:23 -0300

    initial routes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import Routes from './src/routes';
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+        <StatusBar style="light" />
+      </View>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
